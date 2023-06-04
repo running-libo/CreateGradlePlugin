de7b3a7bef82214be8c2cd74d099452df6664382
libo3 <libo3@apusapps.com>
2023-06-04 23:35:35 +0800

    直接引用plugin方式完成

---
+++ buildSrc/src/main/java/com/github/buildsrc/MyPlugin.java
@@ -0,0 +1,12 @@
+package com.github.buildsrc;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class MyPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        System.out.println("This is my plugin");
+    }
+}
